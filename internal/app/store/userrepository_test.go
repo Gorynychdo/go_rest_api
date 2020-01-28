825e00d53f22d8180b345af0ec6d43cb0b8ffe50
igor <gorynychdo@gmail.com>
2020-01-28 16:51:53 +0300

    UserRepository FindByEmail + testing

---
+++ internal/app/store/userrepository_test.go
@@ -0,0 +1,34 @@
+package store_test
+
+import (
+	"github.com/Gorynychdo/go_rest_api/internal/app/model"
+	"github.com/Gorynychdo/go_rest_api/internal/app/store"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email: "user@example.org",
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	email := "user@example.org"
+	_, err := s.User().FindByEmail(email)
+	assert.Error(t, err)
+
+	u, err := s.User().Create(&model.User{
+		Email: "user@example.org",
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
