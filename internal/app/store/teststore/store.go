ee7021478558711285e23c9c954e4f64e95d4244
igor <gorynychdo@gmail.com>
2020-02-11 12:04:59 +0300

    Store interface & server refactoring

---
+++ internal/app/store/teststore/store.go
@@ -0,0 +1,28 @@
+package teststore
+
+import (
+	"github.com/Gorynychdo/go_rest_api/internal/app/model"
+	"github.com/Gorynychdo/go_rest_api/internal/app/store"
+	_ "github.com/lib/pq"
+)
+
+type Store struct {
+	userRepository *UserRepository
+}
+
+func New() *Store {
+	return &Store{}
+}
+
+func (s *Store) User() store.UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		store: s,
+		users: make(map[string]*model.User),
+	}
+
+	return s.userRepository
+}
---
+++ internal/app/store/teststore/userrepository_test.go
@@ -0,0 +1,28 @@
+package teststore_test
+
+import (
+	"github.com/Gorynychdo/go_rest_api/internal/app/model"
+	"github.com/Gorynychdo/go_rest_api/internal/app/store"
+	"github.com/Gorynychdo/go_rest_api/internal/app/store/teststore"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s := teststore.New()
+	u := model.TestUser(t)
+	assert.NoError(t, s.User().Create(u))
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s := teststore.New()
+	email := "user@example.org"
+	_, err := s.User().FindByEmail(email)
+	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
+
+	u := model.TestUser(t)
+	u.Email = "user@example.org"
+	assert.NoError(t, s.User().Create(u))
+	assert.NotNil(t, u)
+}
