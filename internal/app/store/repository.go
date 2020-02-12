93301c58fc91e0fceb948bd0fa0e06d77cc8d647
igor <gorynychdo@gmail.com>
2020-02-12 15:12:11 +0300

    Authentication middleware

--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -4,5 +4,6 @@ import "github.com/Gorynychdo/go_rest_api/internal/app/model"
 
 type UserRepository interface {
 	Create(*model.User) error
+	Find(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 }
